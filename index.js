a06e49b5de7f94ca7034c29e74b251a700d21a82
Svenkatesh18 <76996122+Svenkatesh18@users.noreply.github.com>
2022-08-03 20:24:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// Hamburger/ Navigation toggle button
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener("click", () => {
+    document.body.classList.toggle('nav-open');
+})
+
+navLinks.forEach(link =>{
+    link.addEventListener("click", () =>{
+        document.body.classList.remove('nav-open');
+    })
+})
+
+
+
